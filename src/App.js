1528cdf390e6e97461e12b4ee233091be4b13cbe
Juho Lee <eungseoni@gmail.com>
2023-04-21 00:51:46 +0900

    feat: navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, BrowserRouter, Routes } from "react-router-dom";
+import { useState } from "react";
+import React from "react";
+
+import logo from "./image/ailogo.png";
+import ldw from "./image/ldw.jpg";
+
+import "./App.css";
+import About from "./about/about.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="title">
+      <header className="title-header">
+        <img src={logo} className="title-logo" alt="logo" />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="./about/about.js" element={<About />} />
+          </Routes>
+
+          <Link to="About" className="tltle-text">
+            About
+          </Link>
+        </BrowserRouter>
       </header>
+
+      <body>
+        <div className="body">
+          <img src={ldw} className="body-image" />
+          <p></p>
+        </div>
+      </body>
     </div>
   );
 }
---
+++ src/about/about.js
@@ -0,0 +1,19 @@
+import { Link, Route, BrowserRouter, Routes } from "react-router-dom";
+import { useState } from "react";
+import React from "react";
+
+import "./about.css";
+
+function App() {
+  return (
+    <div className="title">
+      <body>
+        <div className="body">
+          <p>about</p>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
